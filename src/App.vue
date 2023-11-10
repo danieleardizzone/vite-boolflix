64e58c68f8651d423cca6a84e3a31ca5d5d99af4
danieleardizzone <daniele.ardi02@gmail.com>
2023-11-10 15:07:38 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import { store } from './store';
+
+export default {
+  data() {
+    return {
+      store
+    }
+  }, mounted() {
+    console.log(store.msg)
+  }
+}
+</script>
+
+<template></template>
+
+<style scoped></style>
