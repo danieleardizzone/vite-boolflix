397127ec378bbb47f9ee00be88eaec30cde23c1a
danieleardizzone <daniele.ardi02@gmail.com>
2023-11-13 17:51:41 +0100

    ADD - star icons

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -19,42 +19,36 @@ export default {
         },
         imgSrc() {
             return this.store.apiImgUrl + this.item.poster_path
+        },
+        vote_stars() {
+            return Math.round(this.item.vote_average) / 2
         }
     }
 }
 </script>
 
 <template>
-    <div v-if="item.title">
-        <img v-if="item.poster_path !== null" :src="imgSrc" alt="item poster">
-        <h3>
-            {{ item.title }}
-        </h3>
+    <img v-if="item.poster_path !== null" :src="imgSrc" alt="item poster">
+    <h3 v-if="item.title">
+        {{ item.title }}
+    </h3>
+    <h3 v-else>
+        {{ item.name }}
+    </h3>
+    <p v-if="item.original_title">
+        {{ item.original_title }}
+    </p>
+    <p v-else>
+        {{ item.original_name }}
+    </p>
+    <img class="flag" v-if="srcFlag" :src="srcFlag" alt="flag">
+    <p v-else>
+        {{ item.original_language }}
+    </p>
+    <div class="stars">
+        <font-awesome-icon v-for="star in Math.round(vote_stars)" icon="fa-solid fa-star" />
         <p>
-            {{ item.original_title }}
-        </p>
-        <img class="flag" v-if="srcFlag" :src="srcFlag" alt="flag">
-        <p v-else>
-            {{ item.original_language }}
-        </p>
-        <p>
-            {{ item.vote_average }}
-        </p>
-    </div>
-    <div v-else>
-        <img v-if="item.poster_path !== null" :src="imgSrc" alt="item poster">
-        <h3>
-            {{ item.name }}
-        </h3>
-        <p>
-            {{ item.original_name }}
-        </p>
-        <img class="flag" v-if="srcFlag" :src="srcFlag" alt="flag">
-        <p v-else>
-            {{ item.original_language }}
-        </p>
-        <p>
-            {{ item.vote_average }}
+            {{ vote_stars }}
         </p>
     </div>
 </template>
@@ -63,5 +57,15 @@ export default {
 .flag {
     height: 30px;
     aspect-ratio: 1;
+}
+
+.stars {
+    display: flex;
+    gap: 3.5px;
+    color: yellow;
+
+    p {
+        color: black;
+    }
 }
 </style>
